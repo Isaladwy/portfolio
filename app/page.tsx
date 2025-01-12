12230dc748d38084ce94efeece9af24639845f0b
Isaladwy <isaladwy@gmail.com>
2025-01-13 00:30:17 +0200

    Projects section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import About from '@/components/About';
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
-import Work from '@/components/Work';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
-      <Work />
+      <Projects />
     </>
   );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-export default function Work() {
+export default function Projects() {
   return (
     <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
       <h4 className="text-center mb-2 text-lg font-ovo">My portfolio</h4>
-      <h2 className="text-center text-5xl font-ovo">My latest work</h2>
+      <h2 className="text-center text-5xl font-ovo">My latest projects</h2>
 
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
         Welcome to my web development portfolio! Explore a collection of
